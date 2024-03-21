13b230685c20e5611fefe38f6c49c2543bb42c81
Kate test <d.swetlov2017@gmail.com>
2024-03-21 18:20:28 +0300

    mofidied

--- src/test/java/page/DashboardPage.java
+++ src/test/java/page/DashboardPage.java
@@ -34,16 +34,4 @@ public class DashboardPage {
         return Integer.parseInt(value);
     }
 
-    public static class LoginPage {
-        private final SelenideElement loginField = $("[data-test-id=login] input");
-        private final SelenideElement passwordField = $("[data-test-id=password] input");
-        private final SelenideElement loginButton = $("[data-test-id=action-login]");
-
-        public VerificationPage validLogin(DataHelper.AuthInfo info) {
-            loginField.setValue(info.getLogin());
-            passwordField.setValue(info.getPassword());
-            loginButton.click();
-            return new VerificationPage();
-        }
-    }
 }
